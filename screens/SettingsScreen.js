af35e93dc69cd15740ef7e338fdfeafa189ef5be
KKD <kianakdonaldson@gmail.com>
2020-01-20 23:00:50 -0800

    Implement GET request functionality on client side

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,10 +1,37 @@
 import React from 'react';
 import { Image, Text, StyleSheet, ScrollView, View } from 'react-native';
 import userStats from '../userStats.js';
+import { min } from 'moment';
 
-export default function SettingsScreen() {
+export default class SettingsScreen extends React.Component {
 
-  const calcStats = () => {
+  constructor(props) {
+    super(props);
+    this.state = {
+      formData: []
+    }
+  }
+
+  componentDidMount() {
+    return fetch('http://127.0.0.1:3000', {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((response) => response.json())
+      .then((responseJson) => {
+        this.setState({
+          formData: JSON.stringify(responseJson)
+        });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  calcStats() {
     var totals = 0;
     var counter = 0;
 
@@ -22,11 +49,9 @@ export default function SettingsScreen() {
     return percentage;
   }
 
-  const percentageResult = calcStats()
-
-  const styleStats = () => {
+  styleStats() {
     var types = {};
-    
+
     for (var i = 0; i < userStats.length; i++) {
       var stat = userStats[i];
 
@@ -39,58 +64,66 @@ export default function SettingsScreen() {
 
     var typeList = Object.keys(types).slice(0, 3).join(', ');
     return typeList;
-    
   }
 
-  const styleResult = styleStats();
+  minutesStats() {
+    var mins = 0;
+    var type = '';
+    var optimal;
+    var counter = 0;
 
-  // const minutesStats = () => {
-  //   var mins = [];
-
-  //   for (var i = 0; i < userStats.length; i++) {
-  //     var stat = userStats[i];
+    for (var i = 0; i < userStats.length; i++) {
+      var stat = userStats[i];
+      var style = this.styleStats();
+      var styleArr = style.split(', ');
       
-  //     if (stat[minutesSpent] && stat[meditationStyle]) {
-        
-  //     }
+      if (styleArr[0] === stat.meditationStyle) {
+        type = stat.meditationStyle;
+        mins += stat.minutesSpent;
+        counter++
+      }
 
-  //     const mins.sort();
-  //   }
-  // }
+      optimal = mins / counter;
+      optimal = optimal + 15;
+    }
+    
+    return `${optimal} of ${type}`;
+  }
 
-  return (
-    <ScrollView contentContainerStyle={{alignItems: 'center'}}>
-      <Text style={styles.stats}>
-        Your overall mood enhancement:
-      </Text>
-
-      <View style={styles.circle}>
-        <Text style={styles.statNums}>
-          {percentageResult}%
+  render() {
+    return (
+      <ScrollView contentContainerStyle={{ alignItems: 'center' }}>
+        <Text style={styles.stats}>
+          Your overall mood enhancement:
         </Text>
-      </View>
 
-      <Text style={styles.stats}>
-        Your optimal meditation styles:
-      </Text>
+        <View style={styles.circle}>
+          <Text style={styles.statNums}>
+            {this.calcStats()}%
+          </Text>
+        </View>
 
-      <Text style={styles.smallStats}>
-        {styleResult}
-      </Text>
+        <Text style={styles.stats}>
+          Your optimal meditation styles:
+        </Text>
 
-      <Text style={styles.stats}>
-        Your meditative sweet spot to guide you in entering a calm, equanimous space:
-      </Text>
+        <Text style={styles.smallStats}>
+          {this.styleStats()}
+        </Text>
 
-      <Text style={styles.smallStats}>
-        15 | 30 of Vipassana/Insight
-      </Text>
-      <Text style={styles.smallStats}>
-        60 of Movement
-      </Text>
+        <Text style={styles.stats}>
+          Your meditative sweet spot to guide you in entering a calm, equanimous space:
+        </Text>
 
-    </ScrollView>
-  )
+        <Text style={styles.smallStats}>
+          {this.minutesStats()}
+        </Text>
+        <Text style={styles.smallStats}>
+          60 of Movement
+        </Text>
+      </ScrollView>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
